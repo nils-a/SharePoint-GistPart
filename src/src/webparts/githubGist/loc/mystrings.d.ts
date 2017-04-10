84e606f3afb97ce89a8165676df3f1c8a31cd051
Nils Andresen <nils@nils-andresen.de>
2017-04-10 20:48:11 +0200

    clean "yo @microsoft/sharepoint" as of https://dev.office.com/sharepoint/docs/spfx/web-parts/get-started/build-a-hello-world-web-part

---
+++ src/src/webparts/githubGist/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGithubGistStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'githubGistStrings' {
+  const strings: IGithubGistStrings;
+  export = strings;
+}
